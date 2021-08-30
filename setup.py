3e9c5a35676ba07f93202b096cc368bc3f1b5394
xjuan <juanximin@gmail.com>
2021-08-30 22:54:05 +0200

    package created, to be pushlished to pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 30 21:58:08 2021
+
+@author: juanx
+"""
+
+from setuptools import setup
+setup(name = 'delicious_plot',
+     version = '0.1.1',
+     description = 'stacked plot for categorical variables',
+     packages = ['delicious_plot'],
+     zip_safe = False)
